e52df6dcd9b5032ab903ad1711c7fb3c2740c2f7
aman <amanmittal0210@gmail.com>
2024-05-04 17:01:33 +0530

    Added Food Display for Home page

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useState } from 'react'
+import './FoodItem.css'
+import { assets } from '../../assets/assets'
+
+const FoodItem = ({ image, name, price, desc , id }) => {
+
+    return (
+        <div className='food-item'>
+            <div className='food-item-img-container'>
+                <img className='food-item-image' src={image} alt="" />
+            </div>
+            <div className="food-item-info">
+                <div className="food-item-name-rating">
+                    <p>{name}</p> 
+                    <img src={assets.rating_starts} alt="" />
+                </div>
+                <p className="food-item-desc">{desc}</p>
+                <p className="food-item-price">${price}</p>
+            </div>
+        </div>
+    )
+}
+
+export default FoodItem
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
 
@@ -12,6 +12,7 @@ const Home = () => {
       <>
         <Header/>
         <ExploreMenu setCategory={setCategory} category={category}/>
+        <FoodDisplay category={category}/>
       </>
     )
   }
