9fd1d5bfdf28c2d6cc1a12c8efa94b08309ca5db
aman <amanmittal0210@gmail.com>
2024-05-05 15:19:19 +0530

    Added Cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../Context/StoreContext'
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
+
+  const {cartItems, food_list, removeFromCart} = useContext(StoreContext);
+  const navigate = useNavigate();
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p> <p>Title</p> <p>Price</p> <p>Quantity</p> <p>Total</p> <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id]>0) {
+            return (<div key={index}>
+              <div className="cart-items-title cart-items-item">
+                <img src={item.image} alt="" />
+                <p>{item.name}</p>
+                <p>${item.price}</p>
+                <div>{cartItems[item._id]}</div>
+                <p>${item.price*cartItems[item._id]}</p>
+                <p className='cart-items-remove-icon' onClick={()=>removeFromCart(item._id)}>x</p>
+              </div>
+              <hr />
+            </div>)
+          }
+        })}
+      </div>
+    </div>
   )
 }
 
