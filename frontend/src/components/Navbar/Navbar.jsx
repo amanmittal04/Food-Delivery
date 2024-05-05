4f61ada96df1497ea66aac4dc9bfbf91031991dd
aman <amanmittal0210@gmail.com>
2024-05-05 13:25:12 +0530

    Added Login/Sign Up popup

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../../assets/assets'
 import { Link, useNavigate } from 'react-router-dom'
 import { StoreContext } from '../../Context/StoreContext'
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
 
     const [menu, setMenu] = useState("home");
 
@@ -23,7 +23,7 @@ const Navbar = () => {
                 <img src={assets.basket_icon} alt='' />
                 <div className='dot'></div>
             </div>
-            <button>sign in</button>
+            <button onClick={() => setShowLogin(true)}>sign in</button>
         </div>
     </div>
   )
