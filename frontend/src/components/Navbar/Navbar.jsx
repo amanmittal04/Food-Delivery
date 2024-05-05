138a3bd1d713edbab68c93a1461797afca6e8337
aman <amanmittal0210@gmail.com>
2024-05-05 12:43:25 +0530

    Added scroll behavior for navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
 import { Link, useNavigate } from 'react-router-dom'
+import { StoreContext } from '../../Context/StoreContext'
 
 const Navbar = () => {
 
@@ -11,11 +12,11 @@ const Navbar = () => {
     <div className='navbar'>
         <img className='logo' src={assets.logo} alt="" />
         <ul className="navbar-menu">
-            <li onClick={() => setMenu("home")} className={`${menu === "home" ? "active" : ""}`}>home</li>
-            <li onClick={() => setMenu("menu")} className={`${menu === "menu" ? "active" : ""}`}>menu</li>
-            <li onClick={() => setMenu("mobile-app")} className={`${menu === "mobile-app" ? "active" : ""}`}>mobile-app</li>
-            <li onClick={() => setMenu("contact us")} className={`${menu === "contact us" ? "active" : ""}`}>contact us</li>
-        </ul>
+            <Link to="/" onClick={() => setMenu("home")} className={`${menu === "home" ? "active" : ""}`}>home</Link>
+            <a href='#explore-menu' onClick={() => setMenu("menu")} className={`${menu === "menu" ? "active" : ""}`}>menu</a>
+            <a href='#app-download' onClick={() => setMenu("mob-app")} className={`${menu === "mob-app" ? "active" : ""}`}>mobile app</a>
+            <a href='#footer' onClick={() => setMenu("contact")} className={`${menu === "contact" ? "active" : ""}`}>contact us</a>
+      </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className='navbar-search-icon'>
