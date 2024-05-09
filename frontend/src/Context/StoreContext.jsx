bfa81141015b95f30e66549b5adb51f78d3190f4
aman <amanmittal0210@gmail.com>
2024-05-09 20:02:26 +0530

    Added loadCartData function to persist cart data after refreshing browser

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -44,11 +44,17 @@ const StoreContextProvider = (props) => {
         setFoodList(response.data.data)
     }
 
+    const loadCartData = async (token) => {
+        const response = await axios.post(url + "/api/cart/get", {}, { headers: token });
+        setCartItems(response.data.cartData);
+    }
+
     useEffect(() => {
         async function loadData() {
             await fetchFoodList();
             if (localStorage.getItem("token")) {
                 setToken(localStorage.getItem("token"))
+                await loadCartData({ token: localStorage.getItem("token") })
             }
         }
         loadData()
