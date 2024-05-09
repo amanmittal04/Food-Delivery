344532112e39e57f761ec6efce723c827ea5d8db
aman <amanmittal0210@gmail.com>
2024-05-09 19:10:10 +0530

    Created API to remove food item from cart

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addToCart } from '../controllers/cartController.js';
+import { addToCart, removeFromCart } from '../controllers/cartController.js';
 import authMiddleware from '../middleware/auth.js';
 
 const cartRouter = express.Router();
 
 cartRouter.post("/add",authMiddleware,addToCart);
+cartRouter.post("/remove",authMiddleware,removeFromCart);
 
 export default cartRouter;
