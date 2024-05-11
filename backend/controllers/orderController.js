576f2d9622f8335523aa56893c041fdf89120193
aman <amanmittal0210@gmail.com>
2024-05-11 17:29:55 +0530

    corrected amount calculation at payment gateway

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -23,7 +23,7 @@ const placeOrder = async (req, res) => {
               product_data: {
                 name: item.name
               },
-              unit_amount: item.price*100*80
+              unit_amount: item.price*100
             },
             quantity: item.quantity
           }))
@@ -34,7 +34,7 @@ const placeOrder = async (req, res) => {
                 product_data:{
                     name:"Delivery Charge"
                 },
-                unit_amount: 5*80*100
+                unit_amount: 5*100
             },
             quantity:1
         })
