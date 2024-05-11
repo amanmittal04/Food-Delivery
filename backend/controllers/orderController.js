f41872bfc417b6daa5ea51472da76b7939730576
aman <amanmittal0210@gmail.com>
2024-05-11 17:23:20 +0530

    Integrated stripe payment gateway api in frontend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,7 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import 'dotenv/config'
 import Stripe from "stripe";
-
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing User Order for Frontend
